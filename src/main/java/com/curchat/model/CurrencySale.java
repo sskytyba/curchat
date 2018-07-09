3cef47b9832638386f49b43a707af6ebbc928d9e
Serhii Skytyba <serhiiskytyba@fwdext.com>
2018-07-09 11:53:46 +0300

    fix tests error

--- src/main/java/com/curchat/model/CurrencySale.java
+++ src/main/java/com/curchat/model/CurrencySale.java
@@ -22,4 +22,60 @@ public class CurrencySale extends AbstractEntity {
     Currency currencySale;
 
     CurrencySaleStatus status;
+
+    public UserAccount getSeller() {
+        return seller;
+    }
+
+    public void setSeller(UserAccount seller) {
+        this.seller = seller;
+    }
+
+    public UserAccount getBuyer() {
+        return buyer;
+    }
+
+    public void setBuyer(UserAccount buyer) {
+        this.buyer = buyer;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public Currency getCurrencyBuy() {
+        return currencyBuy;
+    }
+
+    public void setCurrencyBuy(Currency currencyBuy) {
+        this.currencyBuy = currencyBuy;
+    }
+
+    public Integer getRate() {
+        return rate;
+    }
+
+    public void setRate(Integer rate) {
+        this.rate = rate;
+    }
+
+    public Currency getCurrencySale() {
+        return currencySale;
+    }
+
+    public void setCurrencySale(Currency currencySale) {
+        this.currencySale = currencySale;
+    }
+
+    public CurrencySaleStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(CurrencySaleStatus status) {
+        this.status = status;
+    }
 }
--- src/test/java/com/curchat/CurchatApplicationTests.java
+++ src/test/java/com/curchat/CurchatApplicationTests.java
@@ -1,12 +1,13 @@
 package com.curchat;
 
+import com.curchat.config.CurchatApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = CurchatApplication.class)
 public class CurchatApplicationTests {
 
 	@Test
