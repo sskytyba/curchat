54ef4b072c4a0eee5adba6d0a4b8eb9d8dd9cbe5
Serhii Skytyba <serhiiskytyba@fwdext.com>
2018-07-09 12:45:03 +0300

    add in-memory database for tests

--- src/test/java/com/curchat/CurchatApplicationTests.java
+++ src/test/java/com/curchat/CurchatApplicationTests.java
@@ -3,11 +3,14 @@ package com.curchat;
 import com.curchat.config.CurchatApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = CurchatApplication.class)
+@SpringBootTest(classes = {CurchatApplication.class})
+@AutoConfigureTestDatabase
 public class CurchatApplicationTests {
 
 	@Test
